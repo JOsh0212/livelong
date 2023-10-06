91d9921e8c6f12ec8731126449cfd122e39fac30
JSH <215212g@gmail.com>
2023-10-06 11:31:25 +0900

    [book-review][fix] 독후감 entity 설정 추가

--- src/main/java/world/neverdie/livelong/bookreview/domain/BookInfo.java
+++ src/main/java/world/neverdie/livelong/bookreview/domain/BookInfo.java
@@ -1,24 +1,69 @@
 package world.neverdie.livelong.bookreview.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import world.neverdie.livelong.bookreview.dto.BookType;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "isbn"),
+        @Index(columnList = "publishDate"),
+        @Index(columnList = "bookCategory"),
+})
 @Entity
 public class BookInfo { //책정보 entity
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    // 책 아이디
-    private String title;   //책 제목
-    private String author;  //작가명 TODO 나중에 작가 테이블 뺄 예정
-    private String translator;  //역자명 TODO 나중에 역자 테이블 뺄 예정
-    private String publisher;   // 출판사
-    private LocalDateTime publishDate;  //출판일자
-    private String thumbnail;   //책 썸네일
-    private String isbn;    //책 ISBN(13)
-    private int bookCategory;   //KDC 번호 TODO 나중에 카테고리 테이블 뺄 예정
+
+    @Setter @Column(nullable = false,length = 1000) private String title;   //책 제목
+    @Setter @Column(nullable = false) private String author;  //작가명 TODO 나중에 작가 테이블 뺄 예정
+    @Setter private String translator;  //역자명 TODO 나중에 역자 테이블 뺄 예정
+    @Setter @Column(nullable = false,length = 100) private String publisher;   // 출판사
+    @Setter @Column(nullable = false) private LocalDateTime publishDate;  //출판일자
+    @Setter @Column(nullable = false) private String thumbnail;   //책 썸네일
+    @Setter @Column(nullable = false,length = 13) private String isbn;    //책 ISBN(13)
+    @Setter private int bookCategory;   //KDC 번호 TODO 나중에 카테고리 테이블 뺄 예정
     @Enumerated(EnumType.STRING) private BookType bookType;    //책 타입
-    private int length; // 쪽수
-    private String firstLine;   //첫문장
-    private String summary; //책 소개 혹은 줄거리
+    @Setter private int length; // 쪽수
+    @Setter private String firstLine;   //첫문장
+    @Setter private String summary; //책 소개 혹은 줄거리
+
+    protected BookInfo() {}
+    private BookInfo(String title, String author, String translator, String publisher, LocalDateTime publishDate, String thumbnail, String isbn, int bookCategory, BookType bookType, int length, String firstLine, String summary) {
+        this.title = title;
+        this.author = author;
+        this.translator = translator;
+        this.publisher = publisher;
+        this.publishDate = publishDate;
+        this.thumbnail = thumbnail;
+        this.isbn = isbn;
+        this.bookCategory = bookCategory;
+        this.bookType = bookType;
+        this.length = length;
+        this.firstLine = firstLine;
+        this.summary = summary;
+    }
+    public static BookInfo of(String title, String author, String translator, String publisher, LocalDateTime publishDate, String thumbnail, String isbn, int bookCategory, BookType bookType, int length, String firstLine, String summary){
+        return new BookInfo(title,author,translator,publisher,publishDate,thumbnail,isbn,bookCategory,bookType,length,firstLine,summary);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(!(o instanceof BookInfo bookInfo)) return false;
+        return id != null && id.equals(bookInfo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/world/neverdie/livelong/bookreview/domain/BookReview.java
+++ src/main/java/world/neverdie/livelong/bookreview/domain/BookReview.java
@@ -1,25 +1,77 @@
 package world.neverdie.livelong.bookreview.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import world.neverdie.livelong.bookreview.dto.ReadingStrategies;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "startedReadingDate"),
+        @Index(columnList = "endReadingDate"),
+        @Index(columnList = "recordDate"),
+        @Index(columnList = "readingStrategies"),
+        @Index(columnList = "oneLineReview"),
+})
 @Entity
 public class BookReview {   // 독후감 테이블
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    //독후감 아이디
-    private Long userId;   // 사용자 아이디 TODO 나중에 사용자 연결할 것
-    private String title;   // 리뷰 타이틀
-    @ManyToOne
-    private BookInfo bookInfo;  // 책정보
-    private LocalDateTime startedReadingDate;   // 읽기 시작한 날
-    private LocalDateTime endReadingDate;   // 다 읽은날
-    private LocalDateTime recordDate;   // 기록날짜
-    private int toolId;    //읽은 방법 TODO 독서도구 -> 책카테고리랑 비슷하게 할 예정
-    private ReadingStrategies readingStrategies;    // 독서 전략
-    private int bookRate;   // 별점 TODO 테이블로 뺄 예정
-    private String oneLineReview;   //한 줄 리뷰
-    private String readingReason; //책 선정이유
-    private String bookContent; //책 비고
+
+    @Setter private Long userId;   // 사용자 아이디 TODO 나중에 사용자 연결할 것
+    @Setter @Column(columnDefinition = "VARCHAR(1000) CHARACTER SET UTF8") private String title;   // 리뷰 타이틀
+    @Setter @ManyToOne(optional = false) private BookInfo bookInfo;  // 책정보
+    @Setter private LocalDateTime startedReadingDate;   // 읽기 시작한 날
+    @Setter private LocalDateTime endReadingDate;   // 다 읽은날
+    @Setter private LocalDateTime recordDate;   // 기록날짜
+    @Setter private int toolId;    //읽은 방법 TODO 독서도구 -> 책카테고리랑 비슷하게 할 예정
+    @Setter private ReadingStrategies readingStrategies;    // 독서 전략
+    @Setter private int bookRate;   // 별점 TODO 테이블로 뺄 예정
+    @Setter @Column(length = 1000) private String oneLineReview;   //한 줄 리뷰
+    @Setter private String readingReason; //책 선정이유
+    @Setter private String bookContent; //책 비고
+
+    //메타데이터
+    @CreatedBy private LocalDateTime createdAt;
+    @LastModifiedDate private LocalDateTime modifiedAt;
+
+    protected BookReview() {}
+
+    private BookReview(Long userId, String title, BookInfo bookInfo, LocalDateTime startedReadingDate, LocalDateTime endReadingDate, LocalDateTime recordDate, int toolId, ReadingStrategies readingStrategies, int bookRate, String oneLineReview, String readingReason, String bookContent) {
+        this.userId = userId;
+        this.title = title;
+        this.bookInfo = bookInfo;
+        this.startedReadingDate = startedReadingDate;
+        this.endReadingDate = endReadingDate;
+        this.recordDate = recordDate;
+        this.toolId = toolId;
+        this.readingStrategies = readingStrategies;
+        this.bookRate = bookRate;
+        this.oneLineReview = oneLineReview;
+        this.readingReason = readingReason;
+        this.bookContent = bookContent;
+    }
+    public static BookReview of(Long userId, String title, BookInfo bookInfo, LocalDateTime startedReadingDate, LocalDateTime endReadingDate, LocalDateTime recordDate, int toolId, ReadingStrategies readingStrategies, int bookRate, String oneLineReview, String readingReason, String bookContent) {
+        return new BookReview(userId, title, bookInfo, startedReadingDate, endReadingDate, recordDate, toolId, readingStrategies, bookRate, oneLineReview, readingReason, bookContent);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BookReview bookReview)) return false;
+        return id!=null && id.equals(bookReview.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
